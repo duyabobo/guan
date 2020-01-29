822bdf5ac18e76b4dee13b886d55e9a144656f7e
duyabo <duyabo@taou.com>
2020-01-29 16:10:03 +0800

    A models.guan_point

--- models.py
+++ models.py
@@ -64,16 +64,26 @@ class GuanAnswer(Base):
     created_time = Column(TIMESTAMP, default=func.now())  # 创建时间
 
 
+class GuanPoint(Base):
+    """关关积分详情表"""
+    __tablename__ = 'guan_point'
+    id = Column(Integer, primary_key=True)  # 自增
+    user_id = Column(Integer)  # user_id
+    guan_id = Column(Integer)  # guanguan id
+    guan_point = Column(Integer)  # 积分数
+    updated_time = Column(TIMESTAMP, default=func.now(), onupdate=func.now())  # 最新更新时间
+    created_time = Column(TIMESTAMP, default=func.now())  # 创建时间
+
+
 class User(Base):
-    """用户基础数据，用户第一次进入产品需要完善一级(或基础)信息：昵称、头像、性别、是否是学生。
-    其中昵称和头像随机自动生成（并可以自主修改），性别、是否是学生是必须自主完善的。
-    """
+    """用户基础数据"""
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)  # 自增
     mobile = Column(String)  # 手机号，也就是账号，数据库中存储的是加密数据  -- index --
     openid = Column(String)  # 微信 openid
     password = Column(String)  # 密码，md5加密后存储  -- index --
     user_status = Column(Integer, default=0)  # 用户状态：见 USER_STATUS  -- index --
+    guan_point = Column(Integer, default=0)  # 积分
     nickname = Column(String, default='')  # 昵称
     profile_photo = Column(String, default='')  # 头像url
     updated_time = Column(TIMESTAMP, default=func.now(), onupdate=func.now())  # 最新更新时间
