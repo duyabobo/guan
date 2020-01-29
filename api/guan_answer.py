c84456804e9fc58070206f57fdb62e9afbf9fefe
duyabo <duyabo@taou.com>
2020-01-29 18:12:56 +0800

    U api/guan_answer: 增加guan_id字段，并支持更新

--- api/guan_answer.py
+++ api/guan_answer.py
@@ -8,6 +8,7 @@ from api.basehandler import BaseHandler
 from dal.answer_info import get_answer_info
 from dal.guan_answer import add_guan_answer
 from dal.guan_answer import get_guan_answer
+from dal.guan_answer import update_guan_answer
 from util.database import object_to_json
 from util.monitor import super_monitor
 
@@ -26,9 +27,17 @@ class GuanAnswerHandler(BaseHandler):
         """
         user_id = self.current_user['id']
         answer_info_id = self.get_request_parameter('answer_info_id', para_type=int)
+        guan_id = self.get_request_parameter('guan_id', para_type=int)
 
         answer_info = get_answer_info(self.db_session, answer_info_id)
-        guan_answer = add_guan_answer(self.db_session, user_id, answer_info.guan_info_id, answer_info_id)
+        guan_info_id = answer_info.guan_info_id
+        guan_answer = get_guan_answer(self.db_session, user_id, guan_info_id)
+        if guan_answer:
+            update_guan_answer(self.db_session, guan_answer, answer_info_id)
+        else:
+            guan_answer = add_guan_answer(
+                self.db_session, user_id, guan_id, guan_info_id, answer_info_id
+            )
         return self.response(
             resp_json={
                 'guan_answer_id': guan_answer.id
--- dal/guan_answer.py
+++ dal/guan_answer.py
@@ -5,21 +5,36 @@
 from models import GuanAnswer
 
 
-def add_guan_answer(db_session, user_id, guan_info_id, answer_info_id):
+def add_guan_answer(db_session, user_id, guan_id, guan_info_id, answer_info_id):
     """
     关关问答回答数据
     :param db_session:
     :param user_id:
+    :param guan_id:
     :param guan_info_id:
     :param answer_info_id:
     :return:
     """
     guan_answer = GuanAnswer(
         user_id=user_id,
+        guan_id=guan_id,
         guan_info_id=guan_info_id,
         answer_info_id=answer_info_id,
     )
     db_session.add(guan_answer)
+    db_session.flush()
+    return guan_answer
+
+
+def update_guan_answer(db_session, guan_answer, answer_info_id):
+    """
+    用户重新回答
+    :param db_session:
+    :param guan_answer:
+    :param answer_info_id:
+    :return:
+    """
+    guan_answer.answer_info_id = answer_info_id
     db_session.flush()
     return guan_answer
 
