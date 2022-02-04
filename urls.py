59b0f9f597861b75b5327ba8a82782e9c9242d0e
duyabo <duyabo@hundun.cn>
2022-02-04 20:10:45 +0800

    rebuild

--- urls.py
+++ urls.py
@@ -3,29 +3,11 @@
 # __author__ = ‘duyabo‘
 # __created_at__ = '2020/1/1'
 from api import example
-from api import guan_answer
-from api import guan_evaluation
-from api import guan_info
-from api import guan_point
-from api import guanguan
-from api import identification
 from api import index
-from api import init
 from api import login
-from api import suggestion
-from api import user
 
 handlers = [
     (r'/$', example.ExampleHandler),
     (r'/index$', index.IndexHandler),
-    (r'/init$', init.InitHandler),
     (r'/login$', login.LoginHandler),
-    (r'/identification$', identification.IdentificationHandler),
-    (r'/guanguan$', guanguan.GuanGuanHandler),
-    (r'/guan_info$', guan_info.GuanInfoHandler),
-    (r'/guan_evaluation$', guan_evaluation.GuanEvaluationHandler),
-    (r'/guan_answer$', guan_answer.GuanAnswerHandler),
-    (r'/guan_point$', guan_point.GuanPointHandler),
-    (r'/user$', user.UserHandler),
-    (r'/suggestion$', suggestion.SuggestionHandler),
 ]
