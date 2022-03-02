f64ae7cc8dc95178193613ab32894341ae3baf6d
duyabo <duyabo@hundun.cn>
2022-03-02 21:52:01 +0800

    发邮件不保证完全可靠

--- service/email_verify.py
+++ service/email_verify.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+import gevent
+
+from model.verify import VerifyModel
 from service import BaseService
+from util.class_helper import lazy_property
 from util.const import RESP_HAS_EMAIL_VERIFY_RECENTLY, RESP_OK, \
     RESP_HAS_EMAIL_VERIFY_FAILED, RESP_HAS_EMAIL_IS_NOT_COMPANY, MODEL_WORK_VERIFY_STATUS_NO
 from util.mail import send_email_verify
 from util.verify import generate_code
-from util.class_helper import lazy_property
-from model.verify import VerifyModel
 
 EMAIL_VERIFY_RECORD = "email_verify_record:{passportId}"
 EMAIL_VERIFY_CODE = "email_verify_code:{passportId}:{email}"
@@ -56,7 +58,7 @@ class EmailVerifyService(BaseService):
         if self.verifyRecord.work_mail != email and self.verifyRecord.work_verify_status == MODEL_WORK_VERIFY_STATUS_NO:
             VerifyModel.fillWorkMail(self.dbSession, self.passportId, email)
         code = generate_code()
-        # 可能有性能问题 todo
-        send_email_verify(email, code)
+        # 可以用celery替代
+        gevent.spawn(send_email_verify, email, code)
         self.setCodeToCache(email, code)
         return RESP_OK
