f3b57d64760672bed419dead63d5407e0e8b56b2
duyabo <duyabo@hundun.cn>
2022-08-20 00:08:47 +0800

    安全下一步要做的事情，不着急

--- model/activity.py
+++ model/activity.py
@@ -16,6 +16,10 @@ from util.ctx import getDbSession
 from util.util_time import datetime2str
 
 
+# todo 安全：
+#  由于openid和email都可以得到很好的保护，活动的时间地点信息是我们最需要保护的。
+#  同时passport/user/requirement/verify/activity五元组数据，需要插入一些假数据。这些数据不是为了让数据好看，而是混淆数据攻击者，防止攻击者对我们用户数据和活动数据进行数据分析。
+#  最近太累了，休息一段时间，再维护这个项目。
 class ActivityModel(BaseModel):
     """活动"""
     __tablename__ = 'activity'
