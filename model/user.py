d1f2c4f9d5df4c56ddb7d00b482518406ae00529
duyabo <duyabo@hundun.cn>
2022-08-20 11:45:58 +0800

    fix

--- model/user.py
+++ model/user.py
@@ -17,7 +17,7 @@ class UserModel(BaseModel):
     id = Column(Integer, primary_key=True)  # 自增
     # has_fill_finish = Column(SmallInteger, default=0)  # 是否完善信息结束
     passport_id = Column(Integer, default=0)  # passport_id
-    sex = Column(String, default="未知")  # 性别：MODEL_SEX_ENUMERATE
+    sex = Column(Integer, default=0)  # 性别：MODEL_SEX_ENUMERATE
     birth_year = Column(Integer, default=0)  # 出生年份
     height = Column(Integer, default=0)  # 身高(厘米)
     weight = Column(Integer, default=0)  # 体重(公斤)
@@ -30,7 +30,7 @@ class UserModel(BaseModel):
     # major = Column(String, default="")  # 专业
     study_from_year = Column(Integer, default=0)  # 入学年份
     # education = Column(String, default="未知")  # 学历枚举
-    martial_status = Column(String, default="未知")  # 婚姻现状：未知，未婚，离异
+    martial_status = Column(Integer, default=0)  # 婚姻现状：未知，未婚，离异
     month_pay = Column(Integer, default=0)  # 月收入(元)
     is_fall_in_love = Column(Integer, default=0)  # 是否坠入爱河
     status = Column(Integer, default=1)  # 逻辑删除标示: MODEL_STATUS_ENUMERATE
