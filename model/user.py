49ca87577bc2f2e54a227e15039143d4a9f49653
duyabo <duyabo@hundun.cn>
2022-09-11 22:26:46 +0800

    加注释

--- model/user.py
+++ model/user.py
@@ -80,7 +80,7 @@ class UserModel(BaseModel):
         return getDbSession().query(cls.id).filter(cls.info_has_filled == match.MODEL_STATUS_YES).count()
 
     @classmethod
-    def getMatchCnt(cls, requirement):
+    def getMatchCnt(cls, requirement):  # 这个方法可能查询会慢，后期会做成异步计算。当下就业形势不好，这个项目今天先来个小结，后续一段时间心思会放到学习量化方面，这个项目除非有特别大的bug或问题，会暂停一段时间。暂停到啥时候呢，疫情结束是个好时候。
         filterParms = [cls.sex == requirement.sex, cls.status == match.MODEL_STATUS_YES]
         if requirement.min_birth_year:
             filterParms.append(cls.birth_year >= requirement.min_birth_year)
