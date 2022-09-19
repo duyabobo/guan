9ea6a8b1f34654abdfb8f24a1cbf8c2ae74abb01
duyabo <duyabo@hundun.cn>
2022-09-20 00:44:31 +0800

    sql

--- sql/20220919.sql
+++ sql/20220919.sql
@@ -1,1 +1,3 @@
 alter table address add column thumbnails_img varchar(32) not null default '' comment '缩略图';
+alter table address modify column thumbnails_img varchar(128) not null default '' comment '缩略图';
+alter table address modify column img varchar(128) not null default '' comment '图片url';
