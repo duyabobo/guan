e9c459be04d0bc0b547c0700c6cf4b8e3ff2b817
duyabo <duyabo@hundun.cn>
2022-10-16 19:48:34 +0800

    fix

--- ral/activity.py
+++ ral/activity.py
@@ -32,20 +32,20 @@ COLUMN_MAP_USER_RANGE = {  # 每个维度对应的取值范围
 
 
 COLUMN_MAP_RERQUIREMENT_RANGE_FUNC = {
-    "mail_type": lambda x: getattr(x, "verify_type"),
-    "sex": lambda x: getattr(x, "sex"),
+    "mail_type": lambda x: [getattr(x, "verify_type"), getattr(x, "verify_type")],
+    "sex": lambda x: [getattr(x, "sex"), getattr(x, "sex")],
     "birth_year": lambda x: range(getattr(x, "min_birth_year"), getattr(x, "max_birth_year")),
     "height": lambda x: range(getattr(x, "min_height"), getattr(x, "max_height")),
     "weight": lambda x: range(getattr(x, "min_weight"), getattr(x, "max_weight")),
     "home_region_id": lambda x: RegionModel.getRegionIdsByRegionId(getattr(x, "home_region_id")),
     "study_from_year": lambda x: range(getattr(x, "min_study_from_year"), getattr(x, "max_study_from_year")),
     "study_region_id": lambda x: RegionModel.getRegionIdsByRegionId(getattr(x, "home_region_id")),
-    "education_level": lambda x: getattr(x, "education_level"),
+    "education_level": lambda x: [getattr(x, "education_level"), getattr(x, "education_level")],
     "education_id": lambda x: EducationModel.getEducationIdsByEducationId(getattr(x, "education_id")),
     "work_region_id": lambda x: RegionModel.getRegionIdsByRegionId(getattr(x, "home_region_id")),
     "work_id": lambda x: WorkModel.getWorkIdsByWorkId(getattr(x, "work_id")),
     "month_pay": lambda x: range(getattr(x, "min_month_pay"), getattr(x, "max_month_pay")),
-    "martial_status": lambda x: getattr(x, "martial_status"),
+    "martial_status": lambda x: [getattr(x, "martial_status"), getattr(x, "martial_status")],
 }
 
 
