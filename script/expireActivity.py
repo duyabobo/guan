a7d7a5128d79f1e5cc76889cffb1b585c129f6c3
duyabo <duyabo@hundun.cn>
2022-10-16 22:16:26 +0800

    sys.path

--- script/expireActivity.py
+++ script/expireActivity.py
@@ -1,8 +1,15 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # 活动过期，清空匹配缓存，每天跑一遍
+import os
+import sys
+
 from model.activity import ActivityModel
 from ral.activity import cleanByActivity
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.dirname(current_dir))
+
 
 if __name__ == "__main__":
     activityIds = ActivityModel.getExpireActivityIdsIn7Days()
