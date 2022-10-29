a380021a98c2d978c4fe7a481c26989a091a075f
duyabo <duyabo@hundun.cn>
2022-10-29 18:35:58 +0800

    CDN_QINIU_ADDRESS_URL

--- util/const/qiniu_img.py
+++ util/const/qiniu_img.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # 七牛
 CDN_QINIU_URL = 'http://img.ggjjzhzz.cn/'
-CDN_QINIU_ADDRESS_URL = 'http://img.ggjjzhzz.cn/address/'
+CDN_QINIU_ADDRESS_URL = 'http://img.ggjjzhzz.cn/'
 CDN_QINIU_BOY_HEAD_IMG = CDN_QINIU_URL + 'miniprogress/user_head/boy.jpg'
 CDN_QINIU_GIRL_HEAD_IMG = CDN_QINIU_URL + 'miniprogress/user_head/girl.jpg'
 CDN_QINIU_UNKNOWN_HEAD_IMG = CDN_QINIU_URL + 'miniprogress/user_head/unknown.jpg'
