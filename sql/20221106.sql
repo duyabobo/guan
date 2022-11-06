b7197800206ea4888038ea705f5c153f6126b18c
duyabo <duyabo@hundun.cn>
2022-11-06 20:06:19 +0800

    qiniu

--- sql/20221106.sql
+++ sql/20221106.sql
@@ -1,1 +1,1 @@
-alter table user add column head_img_obj_name varchar(128) not null default '' comment 'oss的对象名，比如七牛';
+alter table user add column head_img_obj_name varchar(128) not null default '' comment 'oss的对象名，比如七牛' after sex;
