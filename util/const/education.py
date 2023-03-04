8530840dd15b85c5b1cc63a9c703acde35e0174c
duyabo <duyabo@hundun.cn>
2023-03-04 20:32:27 +0800

    feat: EDUCATION_LEVEL

--- util/const/education.py
+++ util/const/education.py
@@ -6,6 +6,6 @@ DEFAULT_MULTI_CHOICE_LIST = [ALL_STR]
 
 DEFAULT_MULTI_INDEX = 0
 
-# 枚举改动，只可追加，不可插入
+# 枚举改动，只可追加，不可插入，如想修改，需要刷数据
 DEFAULT_EDUCATION_INDEX = 0
-EDUCATION_LEVEL = ["未知", "职校", "专科", "三本", "二本", "一本", "211本科", "985本科", "研究生", "211研究生", "985研究生", "博士研究生", "博士后"]
+EDUCATION_LEVEL = ["未知", "高中", "职校", "专科", "本科", "研究生", "博士研究生", "博士后"]
