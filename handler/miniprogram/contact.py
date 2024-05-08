c2cde4ad2d008304988877fe66d2f1c210b3176e
duyabo <duyabo@hundun.cn>
2024-05-08 23:25:40 +0800

    feat: ContactHandler

--- handler/miniprogram/contact.py
+++ handler/miniprogram/contact.py
@@ -3,19 +3,25 @@
 #  Time : 2024/5/8 22:51
 #  File: contact.py
 #  Software: PyCharm
+import util.config
 from handler.basehandler import BaseHandler
 from service.mine import MineService
+from service.wx_crypt.WXBizMsgCrypt import WXBizMsgCrypt
 from util.monitor import superMonitor, Response
 
 
 class ContactHandler(BaseHandler):
     @superMonitor
     def post(self, *args, **kwargs):
-        ToUserName = self.getRequestParameter('ToUserName', '')
-        FromUserName = self.getRequestParameter('FromUserName', '')
-        CreateTime = self.getRequestParameter('CreateTime', 0)
-        MsgType = self.getRequestParameter('MsgType', '')
-        Content = self.getRequestParameter('Content', '')
-        MsgId = self.getRequestParameter('MsgId', '')
-
-        return Response()
+        signature = self.getRequestParameter('signature')
+        timestamp = self.getRequestParameter('timestamp')
+        nonce = self.getRequestParameter('nonce')
+        encrypt_type = self.getRequestParameter('encrypt_type')
+        msgSign = self.getRequestParameter('msg_signature')
+        from_xml = self.request.body_arguments or self.request.body
+        appid = util.config.get('weixin', 'appid')
+        token = util.config.get('weixin', 'token')
+        encodingAESKey = util.config.get('weixin', 'encodingAESKey')
+        decrypt_test = WXBizMsgCrypt(token, encodingAESKey, appid)
+        ret, decrypXml = decrypt_test.DecryptMsg(from_xml, msgSign, timestamp, nonce)
+        return Response(data={'ret': ret, 'decrypXml': decrypXml})
