f6bec51fc17947ce54cb1371a4c198ecaaf7dfbe
duyabo <duyabo@hundun.cn>
2024-12-04 22:35:06 +0800

    feat: 修改图片，不再展示个人头像

--- service/common/guan_helper.py
+++ service/common/guan_helper.py
@@ -20,24 +20,25 @@ class GuanHelper(object):
         """
         thumbnailsFlag: 是否缩略图
         """
-        imgUser = None
-        if currentUser and currentUser.passport_id in [activity.girl_passport_id, activity.boy_passport_id]:  # 自己参与就展示自己头像
-            imgUser = currentUser
-        if matchUser:  # 有异性参与，优先展示异性头像
-            imgUser = matchUser
-
-        if imgUser and imgUser.sex in [MODEL_SEX_MALE_INDEX, MODEL_SEX_FEMALE_INDEX]:  # 人物头像
-            if imgUser.has_head_img != MODEL_STATUS_YES:  # 默认头像
-                return CDN_QINIU_BOY_HEAD_IMG if imgUser.sex == MODEL_SEX_MALE_INDEX else CDN_QINIU_GIRL_HEAD_IMG
-            elif cls.getMatchMeetResult(activity, imgUser) != MODEL_MEET_RESULT_FIT_CHOICE:  # 虚拟头像
-                return cls.getUserImg(imgUser, virtualFlag=True, thumbnailsFlag=thumbnailsFlag)
-            else:  # 真实头像
-                return cls.getUserImg(imgUser, virtualFlag=False, thumbnailsFlag=thumbnailsFlag)
-        else:  # 地址头像
-            if thumbnailsFlag:
-                return address.thumbnails_img
-            else:
-                return address.img
+        # imgUser = None
+        # if currentUser and currentUser.passport_id in [activity.girl_passport_id, activity.boy_passport_id]:  # 自己参与就展示自己头像
+        #     imgUser = currentUser
+        # if matchUser:  # 有异性参与，优先展示异性头像
+        #     imgUser = matchUser
+        #
+        # if imgUser and imgUser.sex in [MODEL_SEX_MALE_INDEX, MODEL_SEX_FEMALE_INDEX]:  # 人物头像
+        #     if imgUser.has_head_img != MODEL_STATUS_YES:  # 默认头像
+        #         return CDN_QINIU_BOY_HEAD_IMG if imgUser.sex == MODEL_SEX_MALE_INDEX else CDN_QINIU_GIRL_HEAD_IMG
+        #     elif cls.getMatchMeetResult(activity, imgUser) != MODEL_MEET_RESULT_FIT_CHOICE:  # 虚拟头像
+        #         return cls.getUserImg(imgUser, virtualFlag=True, thumbnailsFlag=thumbnailsFlag)
+        #     else:  # 真实头像
+        #         return cls.getUserImg(imgUser, virtualFlag=False, thumbnailsFlag=thumbnailsFlag)
+        # else:  # 地址头像
+        #
+        if thumbnailsFlag:
+            return address.thumbnails_img
+        else:
+            return address.img
 
     @classmethod
     def getUserImg(cls, userRecord, virtualFlag, thumbnailsFlag):
